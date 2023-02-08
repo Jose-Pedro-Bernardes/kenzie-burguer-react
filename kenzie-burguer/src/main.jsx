7c6f62cbdbd8e6a2bf1292e219cedcebaf340217
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-08 16:45:31 -0300

    global styles - Reset and Global variables add

--- kenzie-burguer/src/main.jsx
+++ kenzie-burguer/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { ResetStyles } from "./styles/ResetStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ResetStyles />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
---
+++ kenzie-burguer/src/styles/GlobalStyles/index.jsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  :root{
+    --brand-color: #fa6000;
+    
+    --grey-1: rgba(51, 51, 51, 1);
+    --grey-2: rgba(130, 130, 130, 1);
+    --grey-3: rgba(189, 189, 189, 1);
+    --grey-4: rgba(224, 224, 224, 1);
+
+    --heading-3: 1.125rem;
+    --heading-4: 0.875rem;
+    --headline: 1rem;
+    --body: 0.875rem;
+    --caption: 0.75rem;
+
+
+  }
+
+  body {
+    background: rgba(245, 245, 245, 1);
+    font-family: 'Inter', sans-serif;
+  }
+
+  
+`;
