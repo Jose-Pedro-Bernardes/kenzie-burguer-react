d4cc406e29a2b042bd4288ed2fbd09544727d6a7
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-09 18:41:30 -0300

    Input Search - new component add

--- kenzie-burguer/src/requests/AxiosInstance/index.jsx
+++ kenzie-burguer/src/requests/AxiosInstance/index.jsx
@@ -1,8 +1,34 @@
 import axios from "axios";
+import Button from "../../components/Button";
 
-export const axiosInstance = axios.create({
+export const AxiosInstance = axios.create({
   baseURL: "https://hamburgueria-kenzie-json-serve.herokuapp.com/",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+async function renderAllProducts() {
+  try {
+    const response = await AxiosInstance.get("/products");
+    const products = response.data;
+
+    const createProduct = products.map((product) => {
+      <li key={product.id}>
+        <div>
+          <img src={product.img} alt={product.name} />
+        </div>
+        <div>
+          <h2>{product.name}</h2>
+          <p>R${product.category}</p>
+          <h3>
+            {parseFloat(product.price).toFixed(2).toString().replace(".", ",")}
+          </h3>
+        </div>
+        <Button>Adicionar</Button>
+      </li>;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
