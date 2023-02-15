24550df068a33d46ba6aae21a2aff76c97fa3bc0
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-14 22:05:36 -0300

    :)

--- kenzie-burguer/src/requests/AxiosInstance/index.jsx
+++ kenzie-burguer/src/requests/AxiosInstance/index.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import Button from "../../components/Button";
 
 export const AxiosInstance = axios.create({
   baseURL: "https://hamburgueria-kenzie-json-serve.herokuapp.com/",
@@ -7,28 +6,3 @@ export const AxiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-async function renderAllProducts() {
-  try {
-    const response = await AxiosInstance.get("/products");
-    const products = response.data;
-
-    const createProduct = products.map((product) => {
-      <li key={product.id}>
-        <div>
-          <img src={product.img} alt={product.name} />
-        </div>
-        <div>
-          <h2>{product.name}</h2>
-          <p>R${product.category}</p>
-          <h3>
-            {parseFloat(product.price).toFixed(2).toString().replace(".", ",")}
-          </h3>
-        </div>
-        <Button>Adicionar</Button>
-      </li>;
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
-}
